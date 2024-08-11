6a26a542523a2553185674f17dd6561f9b7675a5
Heisjabo <jabojeanmarie5@gmail.com>
2024-08-11 19:31:00 +0200

    text animations

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -6,7 +6,7 @@ type CardProps = {
 
 const Card: React.FC<CardProps> = ({text}) => {
   return (
-    <div className='bg-white px-4  cursor-pointer text-center rounded-[5px] py-12 '>
+    <div  className='bg-white px-4  cursor-pointer text-center rounded-[5px] py-12 '>
       <h4 className='text-[#2245fe] text-xl hover:text-[#161616] font-medium'>{text}</h4>
     </div>
   )
--- src/pages/Waitlist.tsx
+++ src/pages/Waitlist.tsx
@@ -1,25 +1,30 @@
 import Card from "../components/Card";
 import { motion } from "framer-motion";
 
-const variants = {
-  hidden: { opacity: 0, y: 50 },
-  visible: { opacity: 1, y: 0 },
-};
-
 const Waitlist = () => {
   return (
     <div className="waitlist min-h-screen py-6 flex flex-col gap-6 items-center justify-center text-white">
-      <motion.div variants={variants}
-          transition={{ duration: 1, ease: "easeOut" }} className="flex gap-6 flex-col w-[80%] items-center">
-        <motion.h1  variants={variants}
-            transition={{ duration: 1, ease: "easeOut", delay: 0.5 }} className="text-3xl text-center md:text-4xl lg:text-[90px] font-bold">
+      <motion.div
+        
+        className="flex gap-6 flex-col w-[80%] items-center"
+      >
+        <motion.h1 initial={{ y: 90, opacity: 0 }}
+        animate={{ y: 0, opacity: 1.1 }}
+        transition={{ ease: "easeInOut", duration: 1.3 }} className="text-3xl text-center md:text-4xl lg:text-[90px] font-bold">
           Join the waitlist...
         </motion.h1>
-        <p className="text-center text-xl">
+        <motion.p initial={{ y: 60, opacity: 0 }}
+        animate={{ y: 0, opacity: 1.1 }}
+        transition={{ ease: "easeInOut", duration: 1.6 }} className="text-center text-xl">
           Join our beta program to follow the data and find new markets
-        </p>
+        </motion.p>
       </motion.div>
-      <form className="flex md:items-center justify-center mx-auto flex-col md:flex-row w-[80%] gap-3">
+      <motion.form
+        initial={{ y: 0, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+  transition={{ delay: 1 }}
+        className="flex md:items-center justify-center mx-auto flex-col md:flex-row w-[80%] gap-3"
+      >
         <input
           type="text"
           className="py-2 px-3 rounded-[8px] outline-none text-[#333]"
@@ -33,11 +38,29 @@ const Waitlist = () => {
         <button type="submit" className="py-2 px-3 rounded-[8px] bg-[#2245fe]">
           Join Waitlist!
         </button>
-      </form>
+      </motion.form>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-3 py-8 w-[80%] mx-auto lg:grid-cols-3">
-        <Card text="Up-to-date datasets for full market transparency" />
-        <Card text="Connect with 100+ buyers and service providers" />
-        <Card text="Integrated engine for trading, logistics planning and finance" />
+        <motion.div
+          initial={{ y: 100, opacity: 0 }}
+          animate={{ y: 0, opacity: 1.1 }}
+          transition={{ ease: "easeInOut", duration: 0.9 }}
+        >
+          <Card text="Up-to-date datasets for full market transparency" />
+        </motion.div>
+        <motion.div
+          initial={{ y: 100, opacity: 0 }}
+          animate={{ y: 0, opacity: 1.6 }}
+          transition={{ ease: "easeInOut", duration: 1.6 }}
+        >
+          <Card text="Connect with 100+ buyers and service providers" />
+        </motion.div>
+        <motion.div
+          initial={{ y: 100, opacity: 0 }}
+          animate={{ y: 0, opacity: 2 }}
+          transition={{ ease: "easeInOut", duration: 2 }}
+        >
+          <Card text="Integrated engine for trading, logistics planning and finance" />
+        </motion.div>
       </div>
     </div>
   );
